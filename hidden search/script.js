b9ee50538780286e62f03f97ccaf9f049199b4a1
Mena Aziz <menaaziz27@gmail.com>
2022-04-02 17:09:36 +0200

    hidden search widget

---
+++ hidden search/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.btn');
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+
+btn.addEventListener('click', e => {
+	e.preventDefault();
+	search.classList.toggle('active');
+	input.focus();
+});
